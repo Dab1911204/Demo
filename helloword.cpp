f150e89c833167c113caff0ae7c4e99898e5b030
DucAnh <nguyenducanhpx2004@gmail.com>
2025-03-03 20:50:25 +0700

    push code 3

--- helloword.cpp
+++ helloword.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main()
 {
-    cout << "Hello World!";
-    cout << "choducanh";
+    cout << "Hello World!" << endl;
+    cout << "choducanh" << endl;
     cout << "I am learning C++" << endl;
+    cout << "How to use Git" << endl;
     return 0;
 }
